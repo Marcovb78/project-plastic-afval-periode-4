b5b80bfe9d904d7c6d60e5c834565c429b513ece
Robin van der Hoeven <robinvdh2903@gmail.com>
2022-06-18 18:04:36 +0200

    Fixed activities

--- database/seeders/ActivitiesTableSeeder.php
+++ database/seeders/ActivitiesTableSeeder.php
@@ -15,16 +15,16 @@ class ActivitiesTableSeeder extends Seeder
      */
     public function run()
     {
-        activity()->causedBy(auth()->user())
-            ->performedOn(User::find(2))
+        activity()->causedBy(User::find(2))
+            ->performedOn(User::find(1))
             ->log("<span class='wcd-blue'>Wouter</span> heeft de achievement <span class='wcd-pink'>Niet vriendloos</span> behaald.");
 
-        activity()->causedBy(auth()->user())
-            ->performedOn(User::find(4))
+        activity()->causedBy(User::find(4))
+            ->performedOn(User::find(1))
             ->log("<span class='wcd-blue'>Marco</span> heeft de achievement <span class='wcd-pink'>Niet vriendloos</span> behaald.");
 
-        activity()->causedBy(auth()->user())
-            ->performedOn(User::find(3))
+        activity()->causedBy(User::find(3))
+            ->performedOn(User::find(1))
             ->log("<span class='wcd-blue'>Max</span> heeft de achievement <span class='wcd-pink'>Niet vriendloos</span> behaald.");
     }
 }
